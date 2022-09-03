26e4345830b1bb941e1a734827449b07ae1be50d
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-09-03 17:30:50 +0530

    API in upcoming changes

--- scripts/upcoming-launches.js
+++ scripts/upcoming-launches.js
@@ -1,6 +1,20 @@
 document.querySelector(".mid-bar").style.height = window.innerHeight;
 // var countDownDate = new Date("Oct 12, 2021 19:00:00").getTime();
 
+async function getResponse() {
+	const response = await fetch(
+		'https://spacelaunchnow.me/api/ll/2.1.0/launch/upcoming/?format=json',
+		{
+			method: 'GET',
+		}
+	);
+  const data = await response.json();
+  console.log(data.results[0].mission.name);
+}
+
+getResponse();
+
+
 var ids = ["time1", "time2", "time3", "time4", "time5", "time6", "time7", "time8", "time9", "time10"];
 var times=["Oct 12, 2021 19:00:00", 
            "Oct 14, 2021 15:10:00", 
