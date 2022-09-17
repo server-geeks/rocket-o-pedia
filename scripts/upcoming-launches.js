059764d4d7bd500f0bb970a1b36504cc52ed0e1b
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-09-17 21:55:26 +0530

    Added API to HTML

--- scripts/upcoming-launches.js
+++ scripts/upcoming-launches.js
@@ -1,5 +1,6 @@
 document.querySelector(".mid-bar").style.height = window.innerHeight;
 // var countDownDate = new Date("Oct 12, 2021 19:00:00").getTime();
+var times=[];
 
 async function getResponse() {
 	const response = await fetch(
@@ -56,54 +57,62 @@ async function getResponse() {
       </div>
     </div>
     <div class="launchData-right">
-      <span id="time10"><!-- T-3 days 04:46:40 --></span>
+      <span id="time${i+1}"><!-- T-3 days 04:46:40 --></span>
     <span>Window starts ${startdate}</span>
     <span>Window ends ${enddate}</span>
     </div></div>
   `;
   }
+  // for(var i=0; i < data.results.length; i++) {
+  //   times[i] = new Date(data.results[i].net).getTime();
+  // }
+  // console.log(times);
 }
 
 getResponse();
 
 
-var ids = ["time1", "time2", "time3", "time4", "time5", "time6", "time7", "time8", "time9", "time10"];
-var times=["Oct 12, 2021 19:00:00", 
-           "Oct 14, 2021 15:10:00", 
-           "Oct 15, 2021 21:54:00", 
-           "Oct 16, 2021 15:04:00", 
-           "Oct 21, 2021 05:30:00", 
-           "Oct 22, 2021 05:45:00", 
-           "Oct 25, 2021 07:30:00", 
-           "Oct 28, 2021 05:30:00", 
-           "Oct 30, 2021 12:13:00", 
-           "Oct 31, 2021 05:30:00"]
+// var ids = ["time1", "time2", "time3", "time4", "time5", "time6", "time7", "time8", "time9", "time10"];
 
-function setTime(countDownDate, myId){
-    var x = setInterval(function() {
 
-    var now = new Date().getTime();
 
-    var distance = countDownDate - now;
+// function setTime(countDownDate, myId){
+//     var x = setInterval(function() {
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//     var now = new Date().getTime();
 
-    // document.getElementById(myId).innerHTML = "T- " + days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+//     var distance = countDownDate - now;
 
-    if (distance < 0) {
-      clearInterval(x);
-      // document.getElementById(myId).innerHTML = "EXPIRED";
-    }
-  }, 1000);
-}
+    // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    // days = Math.floor( distance / (1000*60*60*24) );
+    // hours = Math.floor( distance / (1000*60*60) );
+    // mins = Math.floor( distance / (1000*60) );
+    // secs = Math.floor( distance / 1000 );
 
-for (var i = 0; i < 10; i++) {
-  setTime(new Date(times[i]).getTime(), ids[i]);
-}
+    // dd = days;
+    // hh = hours - days * 24;
+    // mm = mins - hours * 60;
+    // ss = secs - mins * 60;
+
+//     console.log(dd);
+
+//     document.getElementById(myId).innerHTML = "T- " + dd + "d " + hh + "h "
+//     + mm + "m " + ss + "s ";
+    
+//     if (distance < 0) {
+//       clearInterval(x);
+//       document.getElementById(myId).innerHTML = "EXPIRED";
+//     }
+//   }, 1000);
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   setTime(new Date(times[i]).getTime(), ids[i]);
+// }
 
 document.querySelector(".wt-rocket-head").addEventListener("click", function(){
   window.location.href = "./index.html#rocket-info";
